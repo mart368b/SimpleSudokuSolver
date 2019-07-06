1f290fb183c90f3ea771a576e8be3d282136b52b
Martin Pinholt <mapi@itu.dk>
2019-07-06 14:05:31 +0200

    new board

--- board.py
+++ board.py
@@ -1,13 +1,33 @@
+def get_blanck_board() -> list:
+    return [[None for i in range(0, 9)] for i in range(0, 9)]
+
+def get_solver_board() -> list:
+    return [[set([i for i in range(0, 10)]) for i in range(0, 9)] for i in range(0, 9)]
+
 class Board:
-    def __init__(self):
-        self.board = [[0. for i in range(0, 9)] for i in range(0, 9)]
+    def __init__(self, board = get_blanck_board()):
+        self.board = board
+        self.counter = 0
+        self.solver_board = get_solver_board()
+        self.update_solver()
+
+    def update_solver(self):
+        for y, (row, s_row) in enumerate(zip(self.board, self.solver_board)):
+            for x, cell in enumerate(row):
+                if not cell == None:
+                    self.solver_board[y][x] = None
+                    block = self.get_block(x, y)
+                    self.get_column(x)
 
     def get_block(self, x, y):
-        return [self.board[y][x: x + 3] for i in range(y, y + 3)]
+        block = []
+        for i in range(y, y + 3):
+            block += self.board[i][x: x + 3]
+        return block
 
-    def get_row(self, i: int) -> list:
-        return self.board[i]
+    def get_row(self, y: int) -> list:
+        return self.board[y]
     
-    def get_column(self, i: int) -> list:
-        return [self.board[i][j] for j in range(0, 9)]
-
+    def get_column(self, x: int) -> list:
+        return [self.board[y][x] for y in range(0, 9)]
+    
--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from pprint import pprint
 from board import Board
 
 def run():
-    board = Board()
+    board = Board( board = [
+        [None,None,5,7,None,None,None,None,None], 
+        [4,1,7,None,None,None,None,None,8], 
+        [2,3,None,None,None,None,None,None,7], 
+        [None,None,None,8,None,None,None,2,6], 
+        [9,None,None,None,7,1,3,8,4], 
+        [None,4,None,5,None,None,1,None,None], 
+        [None,None,None,3,None,None,8,None,None], 
+        [None,6,8,None,None,None,None,1,None], 
+        [5,None,4,1,8,None,7,9,3]
+    ])
     pprint(board.board)
-    pprint(board.get_block(0, 0))
+    pprint(board.solver_board)
 
 if __name__ == "__main__":
     run()
